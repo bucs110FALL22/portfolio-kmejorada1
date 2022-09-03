ddea26778be0dcd9945b0ff4152ef9ab91df2913
kmejorada156 <112587065+kmejorada1@users.noreply.github.com>
2022-09-03 01:48:58 +0000

    working with data exercise

---
+++ ch01/exercises/workingwithdata.py
@@ -0,0 +1,17 @@
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15)
+#last equation is not an integer
+
+rate = int(input("What is the current exchange rate from a euro to a U.S. Dollar?"))
+amount = int(input("How much euros would you like to exchange?"))
+total = amount/rate
+result = total + 3
+print(result)
