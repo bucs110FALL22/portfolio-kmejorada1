2954b933273d8642a52369f72b46fd5b90d9dff9
kmejorada156 <112587065+kmejorada1@users.noreply.github.com>
2022-09-10 01:37:30 +0000

    chapter 2 lab

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,25 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+sides = int(input("Input the number of sides you want your shape to have "))
+length = int(input("Input the length you want the sides to have "))
+
+turn = ((sides+1)*(90))/(sides)
+print(turn)
+#while True
+#my_turtle.forward(length)
+#my_turtle.right(turn)
+#my_turtle.forward(length)
+#my_turtle.right(turn)
+#my_turtle.forward(length)
+#my_turtle.right(turn)
+#my_turtle.forward(length)
+#my_turtle.right(turn)
+
+ #   if abs(pos()) < 1:
+ #       break
+
+#window = turtle.Screen()
+#window.bgcolor("orange")
+#window.exitonclick()
