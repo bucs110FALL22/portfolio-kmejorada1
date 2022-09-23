574fbf4021d2ec0d53ab0c37e1aa15c5fdbc4bdd
kmejorada156 <112587065+kmejorada1@users.noreply.github.com>
2022-09-23 23:57:24 +0000

    chapter 3 lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,7 @@
 import turtle #1. import modules
 import random
+import pygame
+import math
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -11,6 +13,7 @@ michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
+turtle.delay(100)
 
 michelangelo.up() # 4. Pick up the pen so we don’t get lines
 leonardo.up()
@@ -18,9 +21,47 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
+mnum = random.randrange(1, 100)
+lnum = random.randrange(1, 100)
+michelangelo.forward(mnum)
+leonardo.forward(lnum)
+michelangelo.goto(-150, 20)
+leonardo.goto(-150,-20)
 
+for x in range (0, 10):
+  mnum = random.randrange(1, 100)
+  lnum = random.randrange(1, 100)
+  michelangelo.forward(mnum)
+  leonardo.forward(lnum)
+
+michelangelo.goto(-150, 20)
+leonardo.goto(-150,-20)
 
 # PART B - complete part B here
+pygame.init()
+window = pygame.display.set_mode((800,800))
+pygame.Color((255,255,255))
 
+def points(num_sides):
+  coords = []
+  num_sides = num_sides
+  side_length = 10
+  offset = 20
 
-window.exitonclick()
+  for i in range (num_sides):
+    theta = (2.0 * math.pi * (i)) / num_sides
+    x = side_length * math.cos(theta) + offset
+    y = side_length * math.sin(theta) + offset
+    coords.append([x, y])
+
+  return coords
+
+point = 3, 4, 6, 8, 360
+for i in (point):
+  pygame.draw.polygon(window, "blue", points(i))
+  pygame.display.flip()
+  pygame.time.delay(1000)
+  window.fill("black")
+  
+#window.exitonclick()
+  
