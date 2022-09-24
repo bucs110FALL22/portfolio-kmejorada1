7d708be3348657f5dc11c62288edb2f62e704dc0
kmejorada156 <112587065+kmejorada1@users.noreply.github.com>
2022-09-24 00:05:35 +0000

    chapter 2 lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,8 +1,11 @@
 import random
 #Part A
 weeks = 16
+print(weeks, type(weeks))
 classes = 5
+print(classes, type(classes))
 tuition = 6000
+print(tuition, type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 classes_per_week = 3
@@ -11,4 +14,4 @@ print(cost_per_class, type(cost_per_class))
 #Part B
 list = [17, 7, 27, 21, 44]
 choice = random.choice(list)
-print(choice)
+print(choice, type(choice))
